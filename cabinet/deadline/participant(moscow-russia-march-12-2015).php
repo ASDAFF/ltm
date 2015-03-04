89d12664585dff1fd4f1451e50b59d981ce86a36
mityamf <mail@ae-studio.ru>
2015-03-04 16:11:44 +0300

    Download files in deadlines for particip

--- cabinet/deadline/participant(moscow-russia-march-12-2015).php
+++ cabinet/deadline/participant(moscow-russia-march-12-2015).php
@@ -1,22 +1,26 @@
 <h3>DEADLINES</h3>
 
+<a href="http://luxurytravelmart.ru/files/DAY1-HB.pdf" target="_blank">Download floor plan for the first day</a>
+<a href="http://luxurytravelmart.ru/files/DAY2-EXHIBITORS.pdf" target="_blank">Download floor plan for the second day</a>
+<a href="http://luxurytravelmart.ru/files/LTM2015_exhibitors_program.pdf" target="_blank">Download program for the event</a>
+
 <p><b>November, 16, 2014:</b> Opening of registration for the hosted buyers.</p>
 <p><b>January, 13, 2015:</b> Opening of registration for the buyers from Moscow.<br />
-Please note that this year registration for the evening reception won’t be open for everyone, access to the evening party is by invitation only.</p>
+Please note that this year registration for the evening reception wonï¿½t be open for everyone, access to the evening party is by invitation only.</p>
 <p><b>February, 10, 2015:</b> Password, user name and detailed instructions on how to use the online system and to set up your appointments will be sent for your attention.</p>
-<p><b>February 12 — March 4, 2015:</b> Scheduling of appointments.</p>
+<p><b>February 12 ï¿½ March 4, 2015:</b> Scheduling of appointments.</p>
 <p><b>March, 6, 2015:</b> Your schedule should be ready and available for printing. A final exhibitors check for the badges.</p>
 
 
 <h3>PROGRAMM OF THE EVENT</h3>
 <h4>Day 1, March 12, 2015 (Thursday)</h4>
-<p>Registration desk will be located in the Foyer of the Ballroom Hall (2nd floor). Organizers’ office – Almaty Hall (2nd floor).</p>
+<p>Registration desk will be located in the Foyer of the Ballroom Hall (2nd floor). Organizersï¿½ office ï¿½ Almaty Hall (2nd floor).</p>
 
-<p><b><i>09:30 – 10:00 – Registration of exhibitors.</i></b></p>
+<p><b><i>09:30 ï¿½ 10:00 ï¿½ Registration of exhibitors.</i></b></p>
 <p>Please contact the registration desk, which will be located in the foyer of the Ballroom (2nd floor);
 you will be provided with a badge and information pack.</p>
 
-<p><b><i>10:00 – 18:00 — Individual pre-scheduled appointments with Hosted Buyers.</i></b></p>
+<p><b><i>10:00 ï¿½ 18:00 ï¿½ Individual pre-scheduled appointments with Hosted Buyers.</i></b></p>
 
 <p>Each appointment will last 10 minutes according to the following scheme (5 minutes added for moving
 from one buyer to another):</p>	
@@ -24,15 +28,15 @@ from one buyer to another):</p>
 <p>10:00 - 10:10 / 10:15 - 10:25 / 10:30 - 10:40 / 10:45 - 10:55 / 11:00 - 11:10<br />
 11:15 - 11:25 / 11:30 - 11:40 / 11:45 - 11:55</p>
 
-<p>11:55 - 12:10 – coffee break</p>
+<p>11:55 - 12:10 ï¿½ coffee break</p>
 
 <p>12:10 - 12:20 / 12:25 - 12:35 / 12:40 - 12:50 / 12:55 - 13:05 / 13:10 - 13:20<br />
 13:25 - 13:35 / 13:40 - 13:50 / 13:55 - 14:05 / 14:10 - 14:20</p>
 
-<p>14:20 – 15:20 – lunch</p>
-<p>15:20 – 15:30 / 15:35 – 15:45 / 15:50 – 16:00 / 16:05 – 16:15 / 16:20 – 16:30<br />
-16:35 – 16:45 / 16:50 – 17:00 / 17:05 – 17:15 / 17:20 – 17:30 / 17:35 – 17:45<br />
-17:50 – 18:00</p>
+<p>14:20 ï¿½ 15:20 ï¿½ lunch</p>
+<p>15:20 ï¿½ 15:30 / 15:35 ï¿½ 15:45 / 15:50 ï¿½ 16:00 / 16:05 ï¿½ 16:15 / 16:20 ï¿½ 16:30<br />
+16:35 ï¿½ 16:45 / 16:50 ï¿½ 17:00 / 17:05 ï¿½ 17:15 / 17:20 ï¿½ 17:30 / 17:35 ï¿½ 17:45<br />
+17:50 ï¿½ 18:00</p>
 
 <p>A bell will ring at the end/start of each appointment.<br />
 Place: Ballroom, Moscow Hall, Washington Hall, Istanbul I & II (2nd floor) and Cafe Russe (1st floor).<br />
@@ -45,37 +49,37 @@ the waiters or our staff if you need anything.</p>
 
 <h4>Day 2, March 13, 2015 (Friday)</h4>
 
-<p>Registration desk will be located in the Foyer of the Ballroom Hall (2nd floor). Organizers’ office –
+<p>Registration desk will be located in the Foyer of the Ballroom Hall (2nd floor). Organizersï¿½ office ï¿½
 Almaty Hall (2nd floor).</p>
 
-<p><b><i>09:30 – 10:00 — Set-up for the 2nd day of LTM.</i></b></p>
+<p><b><i>09:30 ï¿½ 10:00 ï¿½ Set-up for the 2nd day of LTM.</i></b></p>
 
-<p>The set-up is “classroom” format, you will have a table with a sign with your company/hotel name
+<p>The set-up is ï¿½classroomï¿½ format, you will have a table with a sign with your company/hotel name
 on it, and 2 chairs on each side. Power supply will be available at each table (for your laptop
 presentations).<br />
 <b>Wi-Fi connection is available through the hotel.</b><br />
 Please note that wall or standing posters are not allowed; you can only use the space on your table.
-In the event of any damage to hotel property by sticking posters to the wall, you’ll be asked to pay the
+In the event of any damage to hotel property by sticking posters to the wall, youï¿½ll be asked to pay the
 cost of repairs.</p>
 
-<p><b><i>10:00 – 15:50 — Individual pre-scheduled appointments with
+<p><b><i>10:00 ï¿½ 15:50 ï¿½ Individual pre-scheduled appointments with
 buyers from Moscow and Hosted Buyers.</i></b></p>
 
 <p>Each appointment will last 10 minutes according to the following scheme (5 minutes added for moving
 from one exhibitor to another):</p>
 
 <p>
-10:00 – 10:10 / 10:15 – 10:25 / 10:30 – 10:40 / 10:45 – 10:55 / 11:00 – 11:10<br />
-11:15 – 11:25 / 11:30 – 11:40 / 11:45 – 11:55</p>
+10:00 ï¿½ 10:10 / 10:15 ï¿½ 10:25 / 10:30 ï¿½ 10:40 / 10:45 ï¿½ 10:55 / 11:00 ï¿½ 11:10<br />
+11:15 ï¿½ 11:25 / 11:30 ï¿½ 11:40 / 11:45 ï¿½ 11:55</p>
 
-<p>11:55 – 12:10 – coffee break</p>
+<p>11:55 ï¿½ 12:10 ï¿½ coffee break</p>
 
 <p>
-12:10 – 12:20 / 12:25 – 12:35 / 12:40 – 12:50 / 12:55 – 13:05 / 13:10 – 13:20<br />
-13:25 – 13:35 / 13:40 – 13:50 / 13:55 – 14:05 / 14:10 – 14:20 / 14:25 – 14:35<br />
-14:40 – 14:50 / 14:55 – 15:05 / 15:10 – 15:20 / 15:25 – 15:35 / 15:40 – 15:50</p>
+12:10 ï¿½ 12:20 / 12:25 ï¿½ 12:35 / 12:40 ï¿½ 12:50 / 12:55 ï¿½ 13:05 / 13:10 ï¿½ 13:20<br />
+13:25 ï¿½ 13:35 / 13:40 ï¿½ 13:50 / 13:55 ï¿½ 14:05 / 14:10 ï¿½ 14:20 / 14:25 ï¿½ 14:35<br />
+14:40 ï¿½ 14:50 / 14:55 ï¿½ 15:05 / 15:10 ï¿½ 15:20 / 15:25 ï¿½ 15:35 / 15:40 ï¿½ 15:50</p>
 
-<p><b><i>15:50 – 16:30 — light lunch</i></b></p>
+<p><b><i>15:50 ï¿½ 16:30 ï¿½ light lunch</i></b></p>
 
 <p>A bell will ring at the end/start of each appointment.<br />
 Place: Ballroom, Moscow Hall, Washington Hall, Istanbul I & II (2nd floor) and Cafe Russe (1st floor).<br />
@@ -84,7 +88,7 @@ Exhibitors will be seated, and buyers will move around.</p>
 <p>Complementary soft beverages, coffee and tea will be available during the day on request; please ask
 the waiters or our staff if you need anything.</p>
 
-<p><b><i>19:00 – 00:00 Evening Reception and Party</i></b></p>
+<p><b><i>19:00 ï¿½ 00:00 Evening Reception and Party</i></b></p>
 <p>for a highly selected group of professionals from Moscow and exhibitors. All details will be kept secret
 until early March, additional information will be sent for your attention on the 6th of March. Dress-code:
 colorful.<br />
