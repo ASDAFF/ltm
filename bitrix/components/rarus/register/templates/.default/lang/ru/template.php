cce78877a715a2548741e32163c0994b460456ca
Mitya <mail@ae-studio.ru>
2015-05-14 20:41:34 +0300

    guests reg opens

--- bitrix/components/rarus/register/templates/.default/lang/ru/template.php
+++ bitrix/components/rarus/register/templates/.default/lang/ru/template.php
@@ -14,17 +14,14 @@ $MESS["R_EXHIBITIONS_NONE"] = "Exhibitions a missing";
 $MESS["R_E_EXTEND_TITLE"] = "EXHIBITOR";
 $MESS["R_B_EXTEND_TITLE"] = "ГОСТЬ (VIP BUYER, BUYER, HOSTED BUYER)";
 $MESS["R_E_EXHIBITION_TITLE"] = "PLEASE SELECT THE EVENT YOU WOULD LIKE TO REGISTER FOR";
-$MESS["R_B_EXHIBITION_TITLE"] = "<p>Гостевая регистрация  на Luxury Travel Mart Moscow 2015 закончена.</p>
+$MESS["R_B_EXHIBITION_TITLE"] = "<p>Здесь должен быть какой-то приветственный текст.</p>
 
-<p>Свой запрос на участие вы можете направить на электронный адрес <a href='mailto:guests@luxurytravelmart.ru' target='_blank'>guests@luxurytravelmart.ru</a></p>
-<p>Все заявки будут рассмотрены в индивидуальном порядке. Организаторы оставляют за собой право отказать в участии без объяснения причины.</p>
-
-<p>Представители гостиничных групп, индивидуальные отели, зарубежные туроператоры, не участвующие в Luxury Travel Mart, а также PR агентства и маркетинговые компании по оказанию представительских услуг в туриндустрии, рекламные отделы масс-медиа, на выставку не допускаются.</p>";
+<p>Артем, пожалуйста, пришли мне его на мой емейл <a href='mailto:mail@ae-studio.ru' target='_blank'>mail@ae-studio.ru</a></p>";
 
 $MESS["R_E_YOUR_CHOICE"] = "YOUR CHOICE";
 $MESS["R_B_YOUR_CHOICE"] = "ВЫ ВЫБРАЛИ";
-$MESS["R_MORNING"] = ""; /*надпись над галочкой для выбора утра */
-$MESS["R_EVENING"] = "";  /*надпись над галочкой для выбора вечера */
+$MESS["R_MORNING"] = "Утро"; /*надпись над галочкой для выбора утра */
+$MESS["R_EVENING"] = "Вечер";  /*надпись над галочкой для выбора вечера */
 
 $MESS["R_E_SALUTATION"] = "Title";
 
