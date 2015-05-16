dbeb8df0ef5dd701e3da0e9c2de2730880b3e53f
Diana <diana_box@list.ru>
2015-05-16 15:12:00 +0300

    Fix

--- bitrix/components/doka/meetings.all.schedule/component.php
+++ bitrix/components/doka/meetings.all.schedule/component.php
@@ -75,8 +75,10 @@ $arResult['CITY'] = "";
 $exhibitionParam = array();
 $exhibitionParam["IS_HB"] = $arParams["IS_HB"];
 $exhibitionParam["TITLE"] = $arResult["PARAM_EXHIBITION"]["PROPERTIES"]["V_EN"]['VALUE'];
+$exhibitionParam["TITLE_RU"] = $arResult["PARAM_EXHIBITION"]["PROPERTIES"]["V_RU"]['VALUE'];
 if(isset($arParams["IS_HB"]) && $arParams["IS_HB"] == 'Y'){
 	$exhibitionParam["TITLE"] .= " Hosted Buyers session";
+	$exhibitionParam["TITLE_RU"] .= " Hosted Buyers session";
 }
 $exhibitionParam["CUT"] = $arParams['CUT'];
 
