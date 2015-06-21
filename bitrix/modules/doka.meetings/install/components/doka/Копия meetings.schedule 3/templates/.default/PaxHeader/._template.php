dd3ee27289f48e07d2b525fce5bb7e8b8d775990
Diana <diana_box@list.ru>
2015-06-21 20:04:40 +0300

    Some changes

--- bitrix/modules/doka.meetings/install/components/doka/Копия meetings.schedule 3/templates/.default/PaxHeader/._template.php
+++
@@ -1,6 +0,0 @@
-112 path=doka.meetings/install/components/doka/Копия meetings.schedule 3/templates/.default/._template.php
-20 ctime=1387265264
-20 atime=1387265186
-23 SCHILY.dev=16777218
-23 SCHILY.ino=27630475
-18 SCHILY.nlink=1
