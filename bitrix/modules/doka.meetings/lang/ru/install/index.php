b53b9677f23b94674cbb76d0e1ef9167af60da47
Master LTM <info@luxurytravelmart.ru>
2015-06-21 16:12:10 +0000

    Add files

--- bitrix/modules/doka.meetings/lang/ru/install/index.php
+++
@@ -1,6 +0,0 @@
-<?
-$MESS["doka.meetings_MODULE_NAME"] = "Модуль встреч";
-$MESS["doka.meetings_MODULE_DESC"] = "";
-$MESS["doka.meetings_PARTNER_NAME"] = "Webdoka";
-$MESS["doka.meetings_PARTNER_URI"] = "http://www.webdoka.ru/";
-?>
