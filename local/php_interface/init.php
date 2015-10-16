2d73f81dc83cb7f7310d83e4fca64c8fd7086436
Жданок Дмитрий <tvist@sevstar.net.ua>
2015-10-16 19:43:36 +0300

    add clear user props function

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -2,15 +2,16 @@
 require $_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/tools/LuxorConfig.php";
 
 if (file_exists($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/constants.php"))
-    require_once($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/constants.php");
+	require_once($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/constants.php");
 if (file_exists($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/functions.php"))
-    require_once($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/functions.php");
+	require_once($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/functions.php");
 if (file_exists($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/events.php"))
-    require_once($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/events.php");
+	require_once($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/events.php");
 
 CModule::AddAutoloadClasses('',array(
 'CFormMatrix' => "/local/php_interface/lib/CFormMatrix.php",
 'CHandlers' => "/local/php_interface/lib/CHandlers.php",
 'CHLMFunctions' => "/local/php_interface/lib/CHLMFunctions.php",
+'CLTMFunctions' => "/local/php_interface/lib/CLTMFunctions.php",
 ));
 ?>
