b89e721d8149550bf8bf3e404082d061a34d5df4
Diana <diana_box@list.ru>
2015-10-31 16:24:57 +0300

    Add change password script

--- local/php_interface/functions.php
+++ local/php_interface/functions.php
@@ -15,6 +15,22 @@ function makePassCode($passw=""){
 function makePassDeCode($passw=""){
     return passCode(base64_decode($passw), "luxoran");
 }
+function passCodeOld($str, $passw=""){
+    $salt = "Dn8*#2n!9j";
+    $len = strlen($str);
+    $gamma = '';
+    $n = $len>100 ? 16 : 4;
+    while( strlen($gamma)<$len ){
+        $gamma .= substr(pack('H*', sha1($passw.$gamma.$salt)), 0, $n);
+    }
+    return $str^$gamma;
+}
+function makePassCodeOld($passw=""){
+    return base64_encode(passCodeOld($passw, 'luxoran'));
+}
+function makePassDeCodeOld($passw=""){
+    return passCodeOld(base64_decode($passw), "luxoran");
+}
 
 function pre($arr, $name)
 {
---
+++ test/changeOldPass.php
@@ -0,0 +1,27 @@
+<?
+$_SERVER["DOCUMENT_ROOT"] = realpath(dirname(__FILE__)."/..");
+$DOCUMENT_ROOT = $_SERVER["DOCUMENT_ROOT"];
+
+define("NO_KEEP_STATISTIC", true);
+define("NOT_CHECK_PERMISSIONS", true);
+
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+
+$filter = Array("<=ID"=>4886);
+$params = array("SELECT"=>array("UF_PAS"), "FIELDS"=>array("ID","EMAIL"));
+$rsUsers = CUser::GetList(($by="ID"), ($order="desc"), $filter, $params);
+while ($arUser = $rsUsers->Fetch()):
+    if($arUser["ID"] < 4887){
+        $arUser["PASSWORD"] = makePassDeCodeOld($arUser["UF_PAS"]);
+        $arUser["PASSWORD_NEW"] = makePassCode($arUser["PASSWORD"]);
+        echo "<pre>";
+        print_r($arUser);
+        echo "</pre>";
+
+        /*$user = new CUser;
+        $fields = array("UF_PAS"=>$arUser["PASSWORD_NEW"]);
+        $user->Update($arUser["ID"], $fields);*/
+    }
+
+endwhile;
+?>
