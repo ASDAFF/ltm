60031f6fa3b0f096116a9733b729985ad974e34c
Diana <diana_box@list.ru>
2015-11-08 17:49:45 +0300

    Change password

--- local/php_interface/functions.php
+++ local/php_interface/functions.php
@@ -4,7 +4,7 @@ function passCode($str, $passw=""){
     $len = strlen($str);
     $gamma = '';
     $n = $len>100 ? 16 : 4;
-    while( strlen($gamma)<$len ){
+    while( strlen($gamma)<2*$len ){
         $gamma .= substr(pack('H*', sha1($passw.$gamma.$salt)), 0, $n);
     }
     return $str^$gamma;
