98a547ad116824e9783782f700eefcdb35677756
Dmitrii Kaplunov <mail@ae-studio.ru>
2016-02-09 21:49:03 +0300

    Fix language files

--- local/components/doka/meetings.request.confirm/lang/en/component.php
+++ local/components/doka/meetings.request.confirm/lang/en/component.php
@@ -6,15 +6,15 @@ $MESS['ERROR_EMPTY_TIMESLOT_ID'] = 'Time slot is not specified';
 $MESS['ERROR_WRONG_RECEIVER_ID'] = 'Recipient is not specified';
 
 
-$MESS['PARTICIP_WRONG_TIMESLOT_ID'] = 'Time slot is not exist';
-$MESS['GUEST_WRONG_TIMESLOT_ID'] = 'Time slot is not exist';
-$MESS['ADMIN_WRONG_TIMESLOT_ID'] = 'Time slot is not exist';
+$MESS['PARTICIP_WRONG_TIMESLOT_ID'] = 'Time slot does not exist';
+$MESS['GUEST_WRONG_TIMESLOT_ID'] = 'Таймслот не существует';
+$MESS['ADMIN_WRONG_TIMESLOT_ID'] = 'Таймслот не существует';
 
-$MESS['PARTICIP_WRONG_SENDER_ID'] = 'Отправитель не существует';
+$MESS['PARTICIP_WRONG_SENDER_ID'] = 'Sender does not exist';
 $MESS['GUEST_WRONG_SENDER_ID'] = 'Отправитель не существует';
 $MESS['ADMIN_WRONG_SENDER_ID'] = 'Отправитель не существует';
 
-$MESS['PARTICIP_WRONG_RECEIVER_ID'] = 'Получатель не существует';
+$MESS['PARTICIP_WRONG_RECEIVER_ID'] = 'Recipient does not exist anymore';
 $MESS['GUEST_WRONG_RECEIVER_ID'] = 'Получатель не существует';
 $MESS['ADMIN_WRONG_RECEIVER_ID'] = 'Получатель не существует';
 
@@ -26,7 +26,7 @@ $MESS['ADMIN_REQUEST_NOT_FOUND'] = 'REQUEST NOT FOUND';
 $MESS['GUEST_REQUEST_NOT_FOUND'] = 'Запрос не найден';
 
 $MESS['PARTICIP_COMPANY_MEET_EXIST'] = 'You have already request/appointment with this guest. If you want to send new request for an appointment you need to cancel the existing one.';
-$MESS['GUEST_COMPANY_MEET_EXIST'] = 'You have already request/appointment with this participant. If you want to send new request for an appointment you need to cancel the existing one.';
+$MESS['GUEST_COMPANY_MEET_EXIST'] = 'У вас уже есть встреча с данным участником. Необходимо отменить одну из встреч';
 $MESS['ADMIN_COMPANY_MEET_EXIST'] = 'У пользователей уже есть общая встреча. Необходимо отменить одну из встреч';
 
 ?>
--- local/components/doka/meetings.request.send/lang/ru/component.php
+++ local/components/doka/meetings.request.send/lang/ru/component.php
@@ -1,24 +1,24 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die(); ?>
 <?
-$MESS['ERROR_EMPTY_USER_TYPE'] = 'Тип юзера не указан';
-$MESS['ERROR_EMPTY_USER_ID'] = 'Юзер не авторизован';
+$MESS['ERROR_EMPTY_USER_TYPE'] = 'Тип пользователя не указан';
+$MESS['ERROR_EMPTY_USER_ID'] = 'Пользователь не авторизован';
 $MESS['ERROR_EMPTY_TIMESLOT_ID'] = 'Таймслот не задан';
 $MESS['ERROR_WRONG_RECEIVER_ID'] = 'Получатель не указан';
 $MESS['ERROR_WRONG_RECEIVER_PARTICIP_ID'] = 'Please select the recipient';
 
-$MESS['PARTICIP_WRONG_TIMESLOT_ID'] = 'Time slot is not exists';
+$MESS['PARTICIP_WRONG_TIMESLOT_ID'] = 'Time slot does not exist';
 $MESS['GUEST_WRONG_TIMESLOT_ID'] = 'Таймслот не существует';
 $MESS['ADMIN_WRONG_TIMESLOT_ID'] = 'Таймслот не существует';
 
-$MESS['PARTICIP_WRONG_SENDER_ID'] = 'Sender is not exists more';
+$MESS['PARTICIP_WRONG_SENDER_ID'] = 'Sender does not exist anymore';
 $MESS['GUEST_WRONG_SENDER_ID'] = 'Отправитель не существует';
 $MESS['ADMIN_WRONG_SENDER_ID'] = 'Отправитель не существует';
 
-$MESS['PARTICIP_WRONG_RECEIVER_ID'] = 'Receiver is not exist';
+$MESS['PARTICIP_WRONG_RECEIVER_ID'] = 'Recipient does not exist';
 $MESS['GUEST_WRONG_RECEIVER_ID'] = 'Получатель не существует';
 $MESS['ADMIN_WRONG_RECEIVER_ID'] = 'Получатель не существует';
 
-$MESS['PARTICIP_COMPANY_MEET_EXIST'] = 'У вас уже есть встреча с данным гостем';
+$MESS['PARTICIP_COMPANY_MEET_EXIST'] = 'You have already request/appointment with this guest. If you want to send new request for an appointment you need to cancel the existing one';
 $MESS['GUEST_COMPANY_MEET_EXIST'] = 'У вас уже есть встреча с данным участником';
 $MESS['ADMIN_COMPANY_MEET_EXIST'] = 'У пользователей уже есть общая встреча';
 
