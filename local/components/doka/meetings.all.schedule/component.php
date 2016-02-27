f565967bac7f4c28a0fbc777408c7082c1c8f51e
Diana <diana_box@list.ru>
2016-02-27 13:02:37 +0300

    Add php mail

--- local/components/doka/meetings.all.schedule/component.php
+++ local/components/doka/meetings.all.schedule/component.php
@@ -328,6 +328,7 @@ else{
 		"LINK" => "http://".$_SERVER['SERVER_NAME'].$shotPath.strtolower($arParams["EXIB_CODE"]).$isHB.'.zip'
 	);
 	CEvent::SendImmediate("ARCHIVE_READY ", "s1", $arEventFields, $Duplicate = "Y");
+	mail($arParams["EMAIL"], 'PDF расписания',"Архив с выставки\n".$arEventFields["LINK"]);
 }
 
 fullRemove_ff($pdfFolder);
