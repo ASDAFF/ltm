f529d46b630ce18ddaede8fe41b8194b90ebd788
Dmitrii Kaplunov <mail@ae-studio.ru>
2016-03-15 18:14:09 +0300

    Add links

--- local/templates/test/include/ltm_en.php
+++ local/templates/test/include/ltm_en.php
@@ -1,4 +1,4 @@
 <span>LTM GUEST SYSTEM™</span> 
-<p>In order to maximise your experience at Luxury Travel Mart, you may download the LTM app for iOS or Android.</p>
+<p>In order to maximise your experience at Luxury Travel Mart, you may download the LTM app for iOS or Android. <a href="/about-app/">Read more...</a></p>
 <a href="https://appsto.re/ru/wNO-5.i" target="_blank" class="app-store" alt="AppStore" ></a> <a href="https://play.google.com/store/apps/details?id=volpis.com.ltm" target="_blank" class="google-play" alt="Google Play"></a>
 <p class="remark">This app is operational only during LTM events.</p>
