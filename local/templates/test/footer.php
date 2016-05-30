b35311380c16e84bf0a9d02161d6f023cc26717e
Dmitrii Kaplunov <mail@ae-studio.ru>
2016-05-30 21:11:42 +0400

    New groups for last members section

--- local/templates/test/footer.php
+++ local/templates/test/footer.php
@@ -35,7 +35,9 @@ if($exhibCode)
 	".default", 
 	array(
 		"USER_GROUP_ID" => array(
-			0 => "10",
+			0 => "14",
+			1 => "16",
+			2 => "18",
 		),
 		"FORM_FIELD_COMPANY_NAME_ID" => "17",
 		"FORM_FIELD_LOGIN_ID" => "18",
