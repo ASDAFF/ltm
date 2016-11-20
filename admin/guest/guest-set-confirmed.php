68e66ab5d50da0db49f663888ab055d29db94d99
Diana <diana_box@list.ru>
2016-11-20 16:20:37 +0300

    Fixed mail to hb

--- admin/guest/guest-set-confirmed.php
+++ admin/guest/guest-set-confirmed.php
@@ -119,7 +119,8 @@ function confirmUser($userId, $arUserChanges, $unconfirmmedGuestGroupId, $confir
 		CEvent::Send("GUEST_EVENING_CONFIRM", SITE_ID, $arFields, "Y", $templateId);
 	}
 	
-	if(isset($_REQUEST["CONFIRM_UF_HB_{$userId}"]) && isset($_REQUEST["CONFIRM_UF_MR_{$userId}"]) && ("361" == $arExhib["ID"]) or ("488" == $arExhib["ID"])) {
+	if(isset($_REQUEST["CONFIRM_UF_HB_{$userId}"]) && isset($_REQUEST["CONFIRM_UF_MR_{$userId}"]) &&
+		in_array($arExhib["ID"], ["361", "488"])) {
 		CEvent::Send("GUEST_HB_CONFIRM", SITE_ID, $arFields);
 	}
 
