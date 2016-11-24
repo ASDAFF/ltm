48d936148db3bea37943aed5fe28a82e3b93fd5d
Dmitrii Kaplunov <mail@ae-studio.ru>
2016-11-24 21:06:30 +0300

    make prices bold

--- local/templates/test/include/fees.php
+++ local/templates/test/include/fees.php
@@ -5,11 +5,11 @@
   <div class="content pull-overflow">
     <div class="pull-left">
       <div class="pull-overflow fees-list">
-        <p>LTM Moscow Spring 2017 on the 2nd and 3rd of March - 4800 euro (waiting list)</p>
-        <p>LTM Almaty on the 6th of April 2017 - 2500 euro</p>
-        <p>LTM Kiev on the 26th of September 2017 - 2400 euro</p>
-        <p>LTM Almaty on the 29th of September 2017 - 2500 euro</p>
-        <p>LTM Moscow Autumn on the 3rd of October 2017 - 3363 euro</p>
+        <p>LTM Moscow Spring 2017 on the 2nd and 3rd of March - <b>4800 euro (waiting list)</b></p>
+        <p>LTM Almaty on the 6th of April 2017 - <b>2500 euro</b></p>
+        <p>LTM Kiev on the 26th of September 2017 - <b>2400 euro</b></p>
+        <p>LTM Almaty on the 29th of September 2017 - <b>2500 euro</b></p>
+        <p>LTM Moscow Autumn on the 3rd of October 2017 - <b>3363 euro</b></p>
       </div>
     </div>
   </div>
