d38f9ffad63fdc9383150cf103d10a7b8e3b87ee
Diana <diana_box@list.ru>
2017-02-07 23:25:14 +0300

    ltm-120 change cashe time at 600 sec = 10 min

--- cabinet/morning/list/participant.php
+++ cabinet/morning/list/participant.php
@@ -6,7 +6,7 @@ $exhibCode = htmlspecialchars(trim($_REQUEST["EXHIBIT_CODE"]));
 $APPLICATION->IncludeComponent("rarus:session.guest", "",
 	Array(
 			"EXHIB_IBLOCK_ID" => "15",
-			"CACHE_TIME" => 3600,
+			"CACHE_TIME" => 600,
 			"EXHIB_CODE" => $exhibCode,
 			"TYPE" => "MORNING"
 	),
