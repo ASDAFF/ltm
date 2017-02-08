f69f0cb230c15aa56aae2857c645e1e48b82d277
Dmitrii Kaplunov <mail@ae-studio.ru>
2017-02-08 17:51:53 +0300

    some texts changes

--- local/templates/test/include/fees.php
+++ local/templates/test/include/fees.php
@@ -5,11 +5,11 @@
   <div class="content pull-overflow">
     <div class="pull-left">
       <div class="pull-overflow fees-list">
-        <p>LTM Moscow Spring 2017 on the 2nd and 3rd of March - <b>4800 euro (waiting list)</b></p>
         <p>LTM Almaty on the 6th of April 2017 - <b>2500 euro</b></p>
         <p>LTM Kiev on the 26th of September 2017 - <b>2400 euro</b></p>
         <p>LTM Almaty on the 29th of September 2017 - <b>2500 euro</b></p>
         <p>LTM Moscow Autumn on the 3rd of October 2017 - <b>3363 euro</b></p>
+        <p>LTM Moscow Spring 2018 on the 1st and 2nd of March - <b>4800 euro</b></p>
       </div>
     </div>
   </div>
