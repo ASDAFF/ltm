7eacf0ade90e11f2a44041adf17ee0ea2d476dcd
Diana <diana_box@list.ru>
2017-02-23 15:56:52 +0300

    LTM-121 - add new pay requisite

--- local/components/rarus/admin.participant.count/component.php
+++ local/components/rarus/admin.participant.count/component.php
@@ -257,7 +257,8 @@ if($arResult["ERROR_MESSAGE"] == '')
 	     {
 	     	case 0 : $arData["PAY_REQUISITE_XML"] = "IP"; break;
 	     	case 1 : $arData["PAY_REQUISITE_XML"] = "TM"; break;
-			case 2 : $arData["PAY_REQUISITE_XML"] = "EV"; break;
+				case 2 : $arData["PAY_REQUISITE_XML"] = "EV"; break;
+				case 3 : $arData["PAY_REQUISITE_XML"] = "EM"; break;
 	     	default: $arData["PAY_REQUISITE_XML"] = "IP";
 	     }
 
@@ -458,6 +459,29 @@ Account: 40802978702410000010";
 			$data["BENEFICIARY_NAME"] = "Elena V. Vetrova / Ветрова Елена Васильевна";
 		}; break;
 
+			case "EM" : {
+				$data["BENEFICIARY"] = "Travel Media,
+registered as Society with limited liability
+with State Registration Number 1047796617472
+at Federal Tax Service Inspectorate No 46 in Moscow
+Общество с ограниченной ответственностью «Трэвэл Медиа»,
+зарегистрированное Инспекцией Федеральной
+Налоговой Службы № 46 по г. Москве,
+государственный регистрационный номер 1047796617472
+ИНН 7707525284
+
+Moscow, Russia";
+
+
+				$data["BANK_DETAILS"] = "Beneficiary's Bank:
+VTB Bank (open joint-stock company), Moscow, Russia
+SWIFT: VTBRRUMM
+Beneficiary: Travel Media
+Account: 40702978900140010240";
+
+				$data["BENEFICIARY_NAME"] = "Elena Vetrova / Ветрова Елена Васильевна";
+			}; break;
+
     }
 
     global $APPLICATION;
@@ -530,8 +554,15 @@ function ContractPDF($data, &$oPDF, $folder)
 		$img = __DIR__ . '/images/stamp_ev.png';
 		$height = 40;
 		$width = 100.94;
-
 	}
+		elseif($data["PAY_REQUISITE_XML"] == "EM")
+		{
+			$stampY = $oPDF->getY() - 30;
+			$stampX = 130;
+			$img = __DIR__ . '/images/stamp_tm.png';
+			$height = 40;
+			$width = 52.88;
+		}
 
     $oPDF->Image($img,$stampX,$stampY,$width,$height ,'','','',false,72,'',false,false,1);
 
@@ -614,13 +645,6 @@ function FooterPDF($data, &$oPDF, $folder)
     $oPDF->MultiCell(0, 5, "Bank details / Банковские реквизиты:\n", 0, "C");
 
     $oPDF->SetFont('freeserif','',FONT_SIZE);
-    /*
-    $sData  = "Beneficiary's Bank:\n";
-    $sData .= "VTB Bank (open joint-stock company), Moscow, Russia\n";
-    $sData .= "SWIFT: VTBRRUMM\n";
-    $sData .= "Beneficiary: Travel Media\n";
-    $sData .= "Account: 40702978900140010240\n";
-    */
     $oPDF->MultiCell(0, 5, $data["BANK_DETAILS"], 0, "C");
     $oPDF->setXY(0,$oPDF->getY() + LINE_INDENT);
 
@@ -638,10 +662,6 @@ function FooterPDF($data, &$oPDF, $folder)
 
 
     $oPDF->SetFont('freeserif','',FONT_SIZE);
-    /*
-    $sData  = "Elena Vetrova /\n";
-    $sData .= "Ветрова Елена Васильевна\n";
-    */
     $oPDF->MultiCell(0, 5, $data["BENEFICIARY_NAME"], 0, "L");
     $oPDF->setXY(0,$oPDF->getY() + LINE_INDENT);
 
