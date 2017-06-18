85367f69aa673b6e7b35a1ed7b39df02a562e2c5
Diana <diana_box@list.ru>
2017-06-18 16:26:03 +0300

    LTM-147 add template Id to collesgue confirmation

---
+++ local/modules/ltm.domain/lang/ru/options.php
@@ -0,0 +1,7 @@
+<?
+$MESS["BONUS_MIN"] = "Минимальное кол-во баллов";
+$MESS["BONUS_COST"] = "Стоимость балла (руб)";
+$MESS["MAIN_TAB_SET"] = "Настройки";
+$MESS["MAIN_TAB_TITLE_SET"] = "Настройка параметров модуля";
+$MESS["CAL_BONUS_SETTINGS"] = "Настройки бонусной системы";
+?>
