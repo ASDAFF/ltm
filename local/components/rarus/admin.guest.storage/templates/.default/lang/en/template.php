c2adc3383f19aa596d139699fd5b5033fd2af8a6
Dmitrii Kaplunov <mail@ae-studio.ru>
2017-11-02 02:25:39 +0300

    fix typos

--- local/components/rarus/admin.guest.storage/templates/.default/lang/en/template.php
+++ local/components/rarus/admin.guest.storage/templates/.default/lang/en/template.php
@@ -1,13 +1,13 @@
 <?
 $MESS["STORAGE_NO_GUESTS"] = "В хранилище нет гостей";
-$MESS["STORAGE_NO_GUESTS_FOUND"] = "По запосу гостей не найдено";
+$MESS["STORAGE_NO_GUESTS_FOUND"] = "По запросу гостей не найдено";
 $MESS["STORAGE_EMAIL"] = "E-mail";
 $MESS["STORAGE_WORK_COMPANY"] = "Компания";
 $MESS["STORAGE_PERSONAL_PHONE"] = "Телефон";
 $MESS["STORAGE_LOGIN"] = "Логин";
 $MESS["STORAGE_NAME"] = "ФИО";
 $MESS["STORAGE_SEARCH"] = "Поиск";
-$MESS["STORAGE_RESET"] = "Сбосить";
+$MESS["STORAGE_RESET"] = "Сбросить";
 $MESS["STORAGE_ID"] = "ID";
 $MESS["STORAGE_ACTIONS"] = "Действия";
 $MESS["STORAGE_MOVE"] = "Перенести";
