7a13c25c7622a1eee6a8f07d7a577677ecf2bef5
Dmitrii Kaplunov <mail@ae-studio.ru>
2017-11-12 17:54:18 +0300

    Change text

--- registr/yes_g.php
+++ registr/yes_g.php
@@ -5,7 +5,6 @@ $APPLICATION->SetPageProperty("NOT_SHOW_NAV_CHAIN", "N");
 $APPLICATION->SetTitle("THE LEADING LUXURY TRAVEL EXHIBITION");
 ?> 
 <p>Благодарим вас за регистрацию на сайте для участия в выставке<br /><strong>Luxury Travel Mart.</strong></p>
-<p>Ваша заявка на участие рассматривается организаторами выставки.<br /><strong>Уведомление о подтверждении</strong>
-вашего участия будет выслано <strong>в течение 48 часов.</strong></p>
+<p>Ваша заявка на участие рассматривается организаторами выставки. Вы получите уведомление <strong>в течение 48 часов</strong>.</p>
 <p>Список участников и программу мероприятия вы можете посмотреть на сайте.</p>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
