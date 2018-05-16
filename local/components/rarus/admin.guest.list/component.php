73b7e819ba99dc5bf1c6da0b068b2b4a30c669ff
Vova <vz@digital-spectr.ru>
2018-05-16 15:58:19 +0500

    Выведены гости в таблице /admin/ из HL блоков

--- local/components/rarus/admin.guest.list/component.php
+++ local/components/rarus/admin.guest.list/component.php
@@ -1,5 +1,9 @@
 <?
-if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+use Bitrix\Highloadblock as HL;
+use Bitrix\Main\Entity;
+
 global $DB, $USER, $APPLICATION;
 global $sortField, $sortOrder;
 
@@ -9,219 +13,239 @@ isset($arParams["ACT"]) or $arParams["ACT"] = $_REQUEST["ACT"];//off morning eve
 isset($arParams["EXHIBIT_CODE"]) or $arParams["EXHIBIT_CODE"] = $_REQUEST["EXHIBIT_CODE"];
 isset($arParams["GUEST_FORM_ID"]) or $arParams["GUEST_FORM_ID"] = 10;
 
-switch($arParams["ACT"]) {
-	case "spam":case "off": case "morning":case "evening":case "hostbuy": break;
-	default: throw new Exception("Incorrect ACT");
+switch ($arParams["ACT"]) {
+    case "spam":
+    case "off":
+    case "morning":
+    case "evening":
+    case "hostbuy":
+        break;
+    default:
+        throw new Exception("Incorrect ACT");
 }
 
 $arResult = array();
 
-$sortField = $arResult["SORT"] = ($_REQUEST["sort"])?$_REQUEST["sort"]:"COMPANY";
-$sortOrder = $arResult["ORDER"]  = ($_REQUEST["order"])?$_REQUEST["order"]:"asc";
+$sortField = $arResult["SORT"] = ($_REQUEST["sort"]) ? $_REQUEST["sort"] : "COMPANY";
+$sortOrder = $arResult["ORDER"] = ($_REQUEST["order"]) ? $_REQUEST["order"] : "asc";
 
-	if(!CModule::IncludeModule("iblock") || !CModule::IncludeModule("form"))
-	{
-		$this->AbortResultCache();
-		throw new Exception("Can't load modules iblock form");
-	}
+if (!CModule::IncludeModule("iblock") || !CModule::IncludeModule("form") || !CModule::IncludeModule("highloadblock")) {
+    $this->AbortResultCache();
+    throw new Exception("Can't load modules iblock form hlblock");
+}
 
-	//получаем данные по выставке
-	$rs = CIBlockElement::GetList(array("SORT"=>"ASC"),
-	        array("IBLOCK_ID"=>intval($arParams["IBLOCK_ID_EXHIB"]), "CODE"=>$arParams["EXHIBIT_CODE"]/*, "ACTIVE"=>"Y"*/),
-	        false, array("nTopCount"=>1),
-	        array("ID", "NAME", "CODE", "PROPERTY_UC_GUESTS_GROUP", "PROPERTY_GUEST_SPAM_GROUP", "PROPERTY_C_GUESTS_GROUP", "PROPERTY_APP_HB_ID"));
-	if($ar = $rs->GetNext(true, false)) {
-	    $arResult["EXHIB"] = $ar;
-	}
+//получаем данные по выставке
+$rs = CIBlockElement::GetList(array("SORT" => "ASC"),
+    array("IBLOCK_ID" => intval($arParams["IBLOCK_ID_EXHIB"]), "CODE" => $arParams["EXHIBIT_CODE"]/*, "ACTIVE"=>"Y"*/),
+    false, array("nTopCount" => 1),
+    array("ID", "NAME", "CODE", "PROPERTY_UC_GUESTS_GROUP", "PROPERTY_GUEST_SPAM_GROUP", "PROPERTY_C_GUESTS_GROUP", "PROPERTY_APP_HB_ID"));
+if ($ar = $rs->GetNext(true, false)) {
+    $arResult["EXHIB"] = $ar;
+}
 
-	if(!isset($arResult["EXHIB"])) {
-	    throw new Exception("Incorrect exhibbit");
-	}
+if (!isset($arResult["EXHIB"])) {
+    throw new Exception("Incorrect exhibbit");
+}
+//список пользователей
+$arUserFilter = [
+    "GROUPS_ID" => $groupId,
+    "ACTIVE" => "Y"
+];
+$arHlBlockFilter = [
+    "UF_EXHIB_ID" => $arResult["EXHIB"]["ID"],
+];
+$collegueDayTime = false;
+$ufEnum = CUserTypeEntity::GetList( array(), array("ENTITY_ID" => "HLBLOCK_" . $arParams["HLBLOCK_GUEST_COLLEAGUES_ID"], 'LANG' => LANGUAGE_ID, 'FIELD_NAME' => 'UF_DAYTIME') )->Fetch();
+switch ($arParams["ACT"]) {
+    case "off":
+        $groupId = $arResult["EXHIB"]["PROPERTY_UC_GUESTS_GROUP_VALUE"];
+        break;
+    case "spam":
+        $groupId = $arResult["EXHIB"]["PROPERTY_GUEST_SPAM_GROUP_VALUE"];
+        break;
+    case "morning":
+        $groupId = $arResult["EXHIB"]["PROPERTY_C_GUESTS_GROUP_VALUE"];
+        $arUserFilter["UF_MR"] = true;
+        $arHlBlockFilter["UF_MORNING"] = true;
+        $dayTime = CUserFieldEnum::GetList(array(), array(
+            "USER_FIELD_ID" => $ufEnum['ID'],
+            "XML_ID" => strtolower($arParams["ACT"]),
+        ))->Fetch();
+        $collegueDayTime = $dayTime['ID'];
+        break;
+    case "evening":
+        $groupId = $arResult["EXHIB"]["PROPERTY_C_GUESTS_GROUP_VALUE"];
+        $arUserFilter["UF_EV"] = true;
+        $arHlBlockFilter["UF_EVENING"] = true;
+        $dayTime = CUserFieldEnum::GetList(array(), array(
+            "USER_FIELD_ID" => $ufEnum['ID'],
+            "XML_ID" => strtolower($arParams["ACT"]),
+        ))->Fetch();
+        $collegueDayTime = $dayTime['ID'];
+        break;
+    case "hostbuy":
+        $groupId = $arResult["EXHIB"]["PROPERTY_C_GUESTS_GROUP_VALUE"];
+        $arUserFilter["UF_HB"] = true;
+        $arHlBlockFilter["UF_MORNING"] = true;
+        $dayTime = CUserFieldEnum::GetList(array(), array(
+            "USER_FIELD_ID" => $ufEnum['ID'],
+            "XML_ID" => strtolower("evening"),
+        ))->Fetch();
+        $collegueDayTime = $dayTime['ID'];
+        break;
+    default:
+        $groupId = false;
+}
 
-	switch($arParams["ACT"]) {
-		case "off": $groupId = $arResult["EXHIB"]["PROPERTY_UC_GUESTS_GROUP_VALUE"]; break;
-		case "spam": $groupId = $arResult["EXHIB"]["PROPERTY_GUEST_SPAM_GROUP_VALUE"]; break;
-		case "morning":case "evening":case "hostbuy": $groupId = $arResult["EXHIB"]["PROPERTY_C_GUESTS_GROUP_VALUE"]; break;
-		default: $groupId = false;
-	}
 
-    //список пользователей
-    $arUserFilter = [
-      "GROUPS_ID" => $groupId,
-      "ACTIVE" => "Y"
-    ];
-    switch($arParams["ACT"]) {
-    	case "morning": $arUserFilter["UF_MR"] = true; break;
-    	case "evening": $arUserFilter["UF_EV"] = true; break;
-    	case "hostbuy": $arUserFilter["UF_HB"] = true; break;
+
+$userFieldFormAnswer = CFormMatrix::getPropertyIDByExh($arResult["EXHIB"]["ID"]);
+$arUserFilter["!$userFieldFormAnswer"] = false;
+
+$arUserListAll = array();
+$arUserFormAnswersId = array();
+
+$isFiltered = false;
+if (isset($_REQUEST["filter"])) {
+    $arResult["FILTER"]["ID"] = $_REQUEST["ID"];
+    $arResult["FILTER"]["COMPANY"] = $_REQUEST["COMPANY"];
+    $arResult["FILTER"]["REP"] = $_REQUEST["REP"];
+    $arResult["FILTER"]["PHONE"] = $_REQUEST["PHONE"];
+    $arResult["FILTER"]["EMAIL"] = $_REQUEST["EMAIL"];
+    $arResult["FILTER"]["DATE_REGISTER"] = $_REQUEST["DATE_REGISTER"];
+    $arResult["FILTER"]["LOGIN"] = $_REQUEST["LOGIN"];
+
+    if (!empty($arResult["FILTER"]["ID"])) {
+        $arUserFilter["ID"] = $arResult["FILTER"]["ID"];
     }
+    if (!empty($arResult["FILTER"]["LOGIN"])) {
+        $arUserFilter["LOGIN"] = $arResult["FILTER"]["LOGIN"];
+    }
+    if (!empty($arResult["FILTER"]["EMAIL"]) || !empty($arResult["FILTER"]["COMPANY"]) || !empty($arResult["FILTER"]["REP"])
+        || !empty($arResult["FILTER"]["PHONE"]) || !empty($arResult["FILTER"]["DATE_REGISTER"])) {
+        $isFiltered = true;
+    }
+}
 
-    $userFieldFormAnswer = CFormMatrix::getPropertyIDByExh($arResult["EXHIB"]["ID"]);
-    $arUserFilter["!$userFieldFormAnswer"] = false;
-
-    $arUserListAll = array();
-    $arUserFormAnswersId = array();
-
-    $isFiltered = false;
-    if(isset($_REQUEST["filter"])) {
-        $arResult["FILTER"]["ID"] = $_REQUEST["ID"];
-        $arResult["FILTER"]["COMPANY"] = $_REQUEST["COMPANY"];
-        $arResult["FILTER"]["REP"] = $_REQUEST["REP"];
-        $arResult["FILTER"]["PHONE"] = $_REQUEST["PHONE"];
-        $arResult["FILTER"]["EMAIL"] = $_REQUEST["EMAIL"];
-        $arResult["FILTER"]["DATE_REGISTER"] = $_REQUEST["DATE_REGISTER"];
-        $arResult["FILTER"]["LOGIN"] = $_REQUEST["LOGIN"];
-
-        if(!empty($arResult["FILTER"]["ID"])) {
-            $arUserFilter["ID"] = $arResult["FILTER"]["ID"];
+$rsData = CUserTypeEntity::GetList( array(), array("ENTITY_ID" => "HLBLOCK_" . $arParams["HLBLOCK_GUEST_ID"], 'LANG' => LANGUAGE_ID) );
+$arHlBlockInfo = [];
+while($arRes = $rsData->Fetch())
+{
+    $arHlBlockInfo[$arRes["FIELD_NAME"]] = $arRes;
+    if($arRes["USER_TYPE_ID"] == "hlblock"){
+        $hlblock = HL\HighloadBlockTable::getById($arRes["SETTINGS"]["HLBLOCK_ID"])->fetch();
+        $entity = HL\HighloadBlockTable::compileEntity($hlblock);
+        $entity_data_class = $entity->getDataClass();
+        $arrFilter = [];
+        if($arRes["FIELD_NAME"] === "UF_COLLEAGUES"){
+            $arrFilter['UF_DAYTIME'] = $collegueDayTime;
         }
-        if(!empty($arResult["FILTER"]["LOGIN"])) {
-            $arUserFilter["LOGIN"] = $arResult["FILTER"]["LOGIN"];
-        }
-        if(!empty($arResult["FILTER"]["EMAIL"]) || !empty($arResult["FILTER"]["COMPANY"]) || !empty($arResult["FILTER"]["REP"])
-          || !empty($arResult["FILTER"]["PHONE"]) || !empty($arResult["FILTER"]["DATE_REGISTER"])) {
-            $isFiltered = true;
+        $result = $entity_data_class::getList([
+            'filter' => $arrFilter,
+        ]);
+        while ($arElem = $result->Fetch()){
+            if($arRes["FIELD_NAME"] === "UF_COLLEAGUES"){
+                $arHlBlockInfo[$arRes["FIELD_NAME"]]["ITEMS"] = $arElem;
+            }else{
+                $arHlBlockInfo[$arRes["FIELD_NAME"]]["ITEMS"][$arElem["ID"]] = $arElem;
+            }
         }
     }
+}
+$hlblock = HL\HighloadBlockTable::getById($arParams["HLBLOCK_GUEST_ID"])->fetch();
+$entity = HL\HighloadBlockTable::compileEntity($hlblock);
+$entity_data_class = $entity->getDataClass();
+$rsData = $entity_data_class::getList(array(
+    'select' => ['*'],
+    'filter' => $arHlBlockFilter,
+    'order' => ['UF_USER_ID' => 'ASC']
+));
 
-    $rs = CUser::GetList(
-      ($by="ID"),
-      ($order="ASC"),
-      $arUserFilter,
-      array(
-        "SELECT"=>array("UF_*"),
-        "FIELDS"=>array("ID", "LOGIN", "DATE_REGISTER")
-      )
-    );
-    while($ar = $rs->Fetch()) {
-        $ar["REG_DATE"]= $ar["DATE_REGISTER"];
-        $tmpTime = strtotime($ar["REG_DATE"]);
-        $ar["REG_DATE_DATE"] = date("d.m.Y", $tmpTime);
-        $diff = (time() - $tmpTime)/60;//разница в минутах
-        $ar["REG_DIFF"] = floor($diff/60)."ч ".($diff% 60)."м";
+while ($el = $rsData->fetch()) {
 
-    	$arUserListAll[ $ar["LOGIN"] ] = $ar;
-    	$arUserFormAnswersId[ $ar["LOGIN"] ] = $ar[$userFieldFormAnswer];
-    }
-    $arResult["USERS_LIST"] = $arUserListAll;
-    $arUserFormAnswersLoginById = array_flip($arUserFormAnswersId);
+    $user = CUser::GetByID($el["UF_USER_ID"])->Fetch();
+    $user["REG_DATE"] = $user["DATE_REGISTER"];
+    $tmpTime = strtotime($user["REG_DATE"]);
+    $user["REG_DATE_DATE"] = date("d.m.Y", $tmpTime);
+    $diff = (time() - $tmpTime) / 60;//разница в минутах
+    $user["REG_DIFF"] = floor($diff / 60) . "ч " . ($diff % 60) . "м";
 
-     if(empty($arUserListAll)) {
-         echo ("There are 0 users.");
-     }
-
-    if(!empty($arResult["USERS_LIST"])) {
-        //список результатов ответов в форме
-        $arAnswersFilter = array("RESULT_ID"=>implode("|", $arUserFormAnswersId));
-
-        $arAnswers = array();
-        CForm::GetResultAnswerArray(
-            $arParams["GUEST_FORM_ID"],
-            $arResult["GUEST_FORM_QUESTIONS"],
-            $arAnswers,
-            ($b = false),
-            $arAnswersFilter);
-
-        $loginFieldId = CFormMatrix::getFormQuestionIdByFormIDAndQuestionName($arParams["GUEST_FORM_ID"], "LOGIN");
-        $cmpField = CFormMatrix::getFormQuestionIdByFormIDAndQuestionName($arParams["GUEST_FORM_ID"], 0);
-        $phoneField = CFormMatrix::getFormQuestionIdByFormIDAndQuestionName($arParams["GUEST_FORM_ID"], 11);
-        $repNameField = CFormMatrix::getFormQuestionIdByFormIDAndQuestionName($arParams["GUEST_FORM_ID"], 7);
-        $repLNameField = CFormMatrix::getFormQuestionIdByFormIDAndQuestionName($arParams["GUEST_FORM_ID"], 8);
-        $emailField = CFormMatrix::getFormQuestionIdByFormIDAndQuestionName($arParams["GUEST_FORM_ID"], 14);
-
-        if(!$loginFieldId) throw new Exception("Incorrect login field id");
-
-        $arFilterRes = [];
-
-        foreach($arAnswers as $key=>$arAnswer) {
-            $ar = array();
-            foreach($arAnswer as $keyQuestionAnswer=>$arQuestionAnswer) {
-                $arCountElems = count($arQuestionAnswer);
-
-                if($arCountElems > 1) {
-                    $ar[$keyQuestionAnswer] = array();
-                }
-                foreach($arQuestionAnswer as $arEntityAnswer) {
-                    switch($arEntityAnswer["FIELD_TYPE"]) {
-                    	case "checkbox":  $v = (isset($arEntityAnswer["ANSWER_TEXT"]) && trim($arEntityAnswer["ANSWER_TEXT"])) ? $arEntityAnswer["ANSWER_TEXT"] : $arEntityAnswer["TITLE"]; break;
-                    	case "dropdown": $v = $arEntityAnswer["ANSWER_TEXT"]; break;
-                    	default: $v = $arEntityAnswer["USER_TEXT"];
+    foreach ($el as $key => $value) {
+        if(array_key_exists($key, $arHlBlockInfo)){
+            switch ($arHlBlockInfo[$key]["USER_TYPE_ID"]){
+                case "hlblock":
+                    if($key === "UF_COUNTRY"){
+                        if($el["UF_COUNTRY_OTHER"]){
+                            $user[$key] = $el["UF_COUNTRY_OTHER"];
+                        }else{
+                            $user[$key] = $arHlBlockInfo[$key]["ITEMS"][$value]["UF_VALUE"];
+                        }
+                    }elseif($key === "UF_COLLEAGUES"){
+                        $user[$key] = $arHlBlockInfo[$key]["ITEMS"];
+                        foreach ($user[$key] as $field => $elem){
+                            if($field === "UF_SALUTATION"){
+                                $user["COLLEAGUE_" . $field] = $arHlBlockInfo[$field]["ITEMS"][$elem]["UF_VALUE"];
+                            }else{
+                                $user["COLLEAGUE_" . $field] = $elem;
+                            }
+                        }
+                    }elseif(is_array($value)){
+                        $newValues = [];
+                        foreach ($value as $elem){
+                            $newValues[] = $arHlBlockInfo[$key]["ITEMS"][$elem]["UF_VALUE"];
+                        }
+                        $user[$key] = $newValues;
+                    }else{
+                        $user[$key] = $arHlBlockInfo[$key]["ITEMS"][$value]["UF_VALUE"];
                     }
-
-                    if($arCountElems > 1) {
-                        $ar[$keyQuestionAnswer][] = $v;
-                    } else {
-                        $ar[$keyQuestionAnswer] = $v;
+                    break;
+                default:
+                    switch ($key){
+                        case "UF_MORNING":
+                            if($value){
+                                $user[$key] = "Утро";
+                            }
+                            break;
+                        case "UF_EVENING":
+                            if($value){
+                                $user[$key] = "Вечер";
+                            }
+                            break;
+                        default:
+                            $user[$key] = $value;
+                            break;
                     }
-                }
-            }
-
-            //pre($ar);
-
-            //добавляем к инфо пользователя
-            $userLogin = $arUserFormAnswersLoginById[$key];
-            $arResult["USERS_LIST"][$userLogin]["FORM_RESULT_ID"] = $key;
-            if(isset($arResult["USERS_LIST"][$userLogin])) {
-                foreach($ar as $key=>$val) {
-                    $arResult["USERS_LIST"][$userLogin][$key] = $val;
-                }
-            }
-
-            $arResult["USERS_LIST"][$userLogin]["COMPANY"] = $arResult["USERS_LIST"][$userLogin][ $cmpField ];
-            $arResult["USERS_LIST"][$userLogin]["REP"] = $arResult["USERS_LIST"][$userLogin][ $repNameField ]. " ".
-                                                         $arResult["USERS_LIST"][$userLogin][ $repLNameField ];
-            $arResult["USERS_LIST"][$userLogin]["PHONE"] = $arResult["USERS_LIST"][$userLogin][ $phoneField ];
-            $arResult["USERS_LIST"][$userLogin]["EMAIL"] = $arResult["USERS_LIST"][$userLogin][ $emailField ];
-            if($isFiltered) {
-                $addToFilter = true;
-                if(!empty($arResult["FILTER"]["COMPANY"]) &&
-                  strpos(strtolower($arResult["USERS_LIST"][$userLogin]["COMPANY"]), strtolower($arResult["FILTER"]["COMPANY"])) === false) {
-                    $addToFilter = false;
-                }
-                if(!empty($arResult["FILTER"]["DATE_REGISTER"]) &&
-                 strtolower($arResult["USERS_LIST"][$userLogin]["REG_DATE_DATE"]) !==
-                 strtolower($arResult["FILTER"]["DATE_REGISTER"])) {
-                    $addToFilter = false;
-                }
-                //$ar["REG_DATE_DATE"]
-                if(!empty($arResult["FILTER"]["REP"]) &&
-                  strpos(strtolower($arResult["USERS_LIST"][$userLogin]["REP"]), strtolower($arResult["FILTER"]["REP"])) === false) {
-                    $addToFilter = false;
-                }
-                if(!empty($arResult["FILTER"]["PHONE"]) &&
-                  strpos(strtolower($arResult["USERS_LIST"][$userLogin]["PHONE"]), strtolower($arResult["FILTER"]["PHONE"])) === false) {
-                    $addToFilter = false;
-                }
-                if(!empty($arResult["FILTER"]["EMAIL"]) &&
-                  strpos(strtolower($arResult["USERS_LIST"][$userLogin]["EMAIL"]), strtolower($arResult["FILTER"]["EMAIL"])) === false) {
-                    $addToFilter = false;
-                }
-                if($addToFilter) {
-                    $arFilterRes[$userLogin] = $arResult["USERS_LIST"][$userLogin];
-                }
+                    break;
             }
         }
     }
+    $arUserListAll[$user["LOGIN"]] = $user;
+}
+$arResult["USERS_LIST"] = $arUserListAll;
 
-    if($isFiltered) {
-        $arResult["USERS_LIST"] = $arFilterRes;
-        unset($arFilterRes);
-    }
+if (empty($arUserListAll)) {
+    echo("There are 0 users.");
+}
 
-    uasort($arResult["USERS_LIST"], "cmp");
+if ($isFiltered) {
+    $arResult["USERS_LIST"] = $arFilterRes;
+    unset($arFilterRes);
+}
 
-    $res = new CDBResult;
-    $res->InitFromArray($arResult["USERS_LIST"]);
-    $res->NavStart(30); // разбиваем постранично по 30 записей
-    $arResult["NAVIGATE"] = $res->GetPageNavStringEx($navComponentObject, "Пользователи", "");
-    unset($arResult["USERS_LIST"]);
-    while($ar = $res->Fetch()) {
-        $arResult["USERS_LIST"][] = $ar;
-    }
+uasort($arResult["USERS_LIST"], "cmp");
+$res = new CDBResult;
+$res->InitFromArray($arResult["USERS_LIST"]);
+$res->NavStart(30); // разбиваем постранично по 30 записей
+$arResult["NAVIGATE"] = $res->GetPageNavStringEx($navComponentObject, "Пользователи", "");
+unset($arResult["USERS_LIST"]);
+while ($ar = $res->Fetch()) {
+    $arResult["USERS_LIST"][] = $ar;
+}
 
-	$this->IncludeComponentTemplate();
+$this->IncludeComponentTemplate();
 
-function cmp($a, $b){
+function cmp($a, $b)
+{
     global $sortField;
     global $sortOrder;
 
@@ -231,8 +255,8 @@ function cmp($a, $b){
         return 0;
     }
     $res = 1;
-    if($sortOrder != "asc") $res = -1;
-    return ($first < $second) ? -1*$res : 1*$res;
+    if ($sortOrder != "asc") $res = -1;
+    return ($first < $second) ? -1 * $res : 1 * $res;
 }
 
 ?>
