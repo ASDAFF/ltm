2f435c357a1fc6c4b9f03936b83a5746d1d1cfe4
Dmitrii Kaplunov <mail@ae-studio.ru>
2018-05-31 00:11:34 +0400

    change first label in edit form

--- local/components/ds/hl.guest.edit/templates/.default/template.php
+++ local/components/ds/hl.guest.edit/templates/.default/template.php
@@ -34,7 +34,7 @@ $needEvening = $arResult["USER_DATA"]["UF_EVENING"];
         if ($arResult["USER_DATA"]["ID"]) {
             ?>
             <tr>
-                <td width="130">ID:</td>
+                <td width="130">ID результата:</td>
                 <td><?= $arResult["USER_DATA"]["ID"] ?></td>
             </tr>
             <?
