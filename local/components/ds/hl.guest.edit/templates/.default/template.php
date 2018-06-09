1cc1c5bf793fed4251ce659d51a86ab99e3f90a0
Amf1k <vz@digital-spectr.ru>
2018-06-09 16:11:12 +0500

    Revert "LTM-30 || Добавлено поле "зал", выведено редактирования у админа и в таблице"
    This reverts commit 2cb011e

--- local/components/ds/hl.guest.edit/templates/.default/template.php
+++ local/components/ds/hl.guest.edit/templates/.default/template.php
@@ -2,6 +2,7 @@
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 $needMorning = $arResult["USER_DATA"]["USER"]["UF_MR"];
 $needEvening = $arResult["USER_DATA"]["USER"]["UF_EV"];
+c($arResult["FIELD_DATA"]);
 ?>
 <form action="<?=$arResult['FORM_URL']?>" method="POST" enctype="multipart/form-data">
     <?= bitrix_sessid_post() ?>
