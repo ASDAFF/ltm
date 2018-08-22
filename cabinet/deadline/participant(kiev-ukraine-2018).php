9fc94b95544023042c7f7f405fb927a43318be15
Dmitrii Kaplunov <mail@ae-studio.ru>
2018-08-23 00:30:46 +0300

    Changed deadlines texts

---
+++ cabinet/deadline/participant(kiev-ukraine-2018).php
@@ -0,0 +1,55 @@
+<h3>DEADLINES</h3>
+<p><b>July, 3, 2018:</b> Opening of registration for the buyers.</p>
+<p><b>September, 3, 2018:</b> Password, user name and detailed instructions on how to use the online system and to set up your appointments will be sent for your attention.</p>
+<p><b>September 4-18, 2018:</b> Scheduling of appointments.</p>
+<p><b>September, 18, 2018:</b> Your schedule should be ready and available for printing. Final exhibitors' check.</p>
+
+<h3>PROGRAMM OF THE EVENT</h3>
+<p>Registration desk will be located in the foyer of the Grand Ballroom Hall ( -2nd floor).</p>
+
+
+<p><b class="bluecolor">10:00 - 12:55</b> Individual pre-scheduled appointments.</p>
+
+<p>Individual pre-scheduled appointments with top-buyers. Each appointment will last 10 minutes according to the following scheme (5 minutes added for moving from one exhibitor to the next):</p>
+
+<p class="bluecolor">10:00 - 10:10 / 10:15 - 10:25 / 10:30 - 10:40 / 10:45 - 10:55<br>
+11:00 - 11:10 / 11:15 - 11:25 / 11:30 - 11:40 / 11:45 - 11:55<br>
+12:00 - 12:10 / 12:15 - 12:25 / 12:30 - 12:40 / 12:45 - 12:55</p>
+
+<p><b class="bluecolor">12:55 - 14:00</b> Lunch. <i>Place: foyer of the Grand Ballroom Hall ( -2nd floor).</i></p>
+
+<p><b class="bluecolor">14:00 - 16:55</b> Individual pre-scheduled appointments.</p>
+
+<p class="bluecolor">14:00 - 14:10 / 14:15 - 14:25 / 14:30 - 14:40 / 14:45 - 14:55<br>
+15:00 - 15:10 / 15:15 - 15:25 / 15:30 - 15:40 / 15:45 - 15:55<br>
+16:00 - 16:10 / 16:15 - 16:25 / 16:30 - 16:40 / 16:45 - 16:55</p>
+
+<p>A bell will ring at the end/start of each appointment.</p>
+
+<p>Complementary soft beverages, coffee and tea will be available during the day on request; please ask the waiters or our staff if you will need anything.</p>
+
+<p><i>Place: Grand Ballroom Hall, Conference Hall I&II ( -2nd floor).</i></p>
+
+
+
+<p><b class="bluecolor">19:00 - 22:00</b> Evening reception.</p>
+
+
+<p>Cocktail reception for travel agents, corporate clients and journalists in a relaxed atmosphere. <i>Place: Grand Ballroom Hall.</i></p>
+
+
+<!-- <h3>Files for Download</h3>
+
+<ul>
+	<li><a href="/files/program_Kiev_upd_28Aug.pdf" title="" target="_blank">Download detailed program</a></li>
+	<li><a href="/files/kiev_morning_evening.pdf" target="_blank" title="">Download floor plan and table assignment for the morning session</a></li>
+
+	<li><a href="/files/kiev_hb.pdf" title="" target="_blank">Download floor plan and table assignment for the hosted buyers session</a></li>
+</ul> -->
+<div class="signature">
+	<p>All events are available only to pre-registered visitors.<br>
+		Non-participating hoteliers, DMC companies and other hospitality providers will not be permitted to attend the <b>Luxury Travel Mart</b> Kyiv as visitors.</p>
+	<p>We kindly ask you NOT TO INVITE any of your colleagues from other hotels, cruise companies, DMCs, etc as there might be a conflict of interests with other exhibitors. </p>
+	<p>Thank you for your understanding.</p>
+</div>
+
