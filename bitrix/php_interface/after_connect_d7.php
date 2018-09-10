9b5ad151767ef9a9aa8a9f4c904ed04e96ad5d3d
Server's side LTM <bitrix@luxurytravelmart.ru>
2018-09-10 14:20:31 +0300

    Changes on the server's side

--- bitrix/php_interface/after_connect_d7.php
+++ bitrix/php_interface/after_connect_d7.php
@@ -1,10 +1,5 @@
 <?
 $connection = \Bitrix\Main\Application::getConnection();
-// $connection->queryExecute("SET NAMES 'cp1251'");
-
 $connection->queryExecute("SET NAMES 'utf8'");
 $connection->queryExecute('SET collation_connection = "utf8_unicode_ci"');
-
-$connection = Bitrix\Main\Application::getConnection();
 $connection->queryExecute("SET LOCAL time_zone='".date('P')."'");
-?>
--- local/php_interface/after_connect_d7.php
+++ local/php_interface/after_connect_d7.php
@@ -7,4 +7,3 @@ $connection->queryExecute('SET collation_connection = "utf8_unicode_ci"');
 
 $connection = Bitrix\Main\Application::getConnection();
 $connection->queryExecute("SET LOCAL time_zone='".date('P')."'");
-?>
