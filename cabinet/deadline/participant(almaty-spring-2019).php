e70d8b5ad0f2840181c863b607416e9ec4765c77
Dmitrii Kaplunov <mail@ae-studio.ru>
2019-02-01 02:56:47 +0300

    deadlines

---
+++ cabinet/deadline/participant(almaty-spring-2019).php
@@ -0,0 +1,22 @@
+<h3>Your action is required on the following:</h3>
+
+<p>Please book your accommodation by the <b>1st of February 2019.</b></p>
+
+<p>Please request an invitation letter and apply for an entry visa for Kazakhstan (if required). Please note that it may take a long time to receive an entry visa to Kazakhstan, so start the process as soon as you can. </p>
+
+<p>Please check that all your tickets and transfers are booked and all travel arrangements are in order. If you sent your brochures in advance, please check that they have arrived safely and are not stuck in customs. </p>
+
+<p><b>November 12, 2018:</b> Opening of registration for the buyers.</p>
+
+<p><b>On the February 11, 2019</b> you will receive an access code for your personal web-page on the <b>LTM</b> web-site, where you can schedule appointments with buyers, upload info for the catalogue, exchange messages with buyers, etc. </p>
+
+<p><b>February 12 – 25, 2019</b> – scheduling of appointments. Please note that it is your own responsibility to maintain the schedule. If you forget or are too late to schedule and it turns out that you don’t have any pre-scheduled appointments – we will not accept any responsibility for this. </p>
+
+<p><b>February 26, 2019:</b> Your schedule should be ready and available for printing. Final exhibitors’ check. </p>
+
+<p><b>Please do not invite any partners of your company/hotel to the Luxury Travel Mart without receiving approval from the organizers in advance. </b></p>
+
+<p>
+	<a href="/files/almaty-spring-2019/Almaty_2019-exhibitors.pdf" target="_blank">Please download the detailed program of the event.</a>
+</p>
+
---
+++ cabinet/deadline/participant(moscow-spring-2019).php
@@ -0,0 +1,27 @@
+<h3>Your action is required on the following:</h3>
+
+<p>Please book your accommodation with Hyatt Regency Moscow Petrovsky Park or any other hotel by your choice by the <b>28th of January 2019.</b> </p>
+
+<p>Please request an invitation letter and apply for an entry visa for Russia (if required). Please note that it may take a long time to receive an entry visa to Russia, so start the process as soon as you can. </p>
+
+<p>Please check that all your tickets and transfers are booked and all travel arrangements are in order. If you sent your brochures in advance, please check that they have arrived safely and are not stuck in customs. </p>
+
+<p><b>November 12, 2018:</b> Opening of registration for the buyers. </p>
+
+<p>On the <b>4th of February 2019</b> you will receive an access code for your personal web-page on the LTM web-site, where you can schedule appointments with buyers, upload info for the catalogue, exchange messages with buyers, etc. </p>
+
+<p>From the <b>5th to the 12th of February 2019</b> — scheduling of appointments for the 1st day of LTM, from the <b>12th to the 20th of February 2019</b> scheduling of appointments for the 2nd day of LTM.</p>
+
+<p>Please note that it is your own responsibility to maintain the schedule. If you forget or are too late to schedule and it turns out that you don’t have any pre-scheduled appointments – we will not accept any responsibility for this. </p>
+
+<p><b>Please do not invite any partners of your company/hotel to the Luxury Travel Mart without receiving approval from the organizers in advance.</b></p>
+
+
+
+
+
+<p>
+	<a href="/files/moscow-spring-2019/Moscow_2019-exhibitors.pdf" target="_blank">Please download the detailed program of the event.</a>
+</p>
+
+
