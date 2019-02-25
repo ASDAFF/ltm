6e7aca4cc18a8b54fa1b2461e55396a891575ec0
Dmitrii Kaplunov <mail@ae-studio.ru>
2019-02-26 01:30:37 +0300

    changed teaser

--- index_popup.php
+++ index_popup.php
@@ -1,30 +1,17 @@
 <div class="main-modal">
     <div class="main-modal__close"></div>
-    <div class="main-modal__header">			
-        <img src="<?=SITE_TEMPLATE_PATH?>/images/logo-modal.svg" alt="" class="main-modal__logo">
-        <h1 class="main-modal__title">LTM Mobile App</h1>
-    </div>
-
-    <div class="main-modal__content">
-        <h2 class="main-modal__subheader">Now available in the Apple AppStore and on Google Play Market</h2>
-        <p class="main-modal__main-text">Search for "LTM" and download the App to use at this and other Luxury Travel Mart events</p>			    	
-        <div class="main-modal__wrapper">
-            <p class="main-modal__list-caption">With the LTM Mobile App you can:</p>
-            <ul class="main-modal__items">
-                <li class="main-modal__item">check your schedule</li>
-                <li class="main-modal__item">see who is around you</li>
-                <li class="main-modal__item">message buyers/exhibitors</li>
-                <li class="main-modal__item">add people to favourites</li>
-                <li class="main-modal__item">call buyers to make an appointment</li>
-                <li class="main-modal__item">report no-shows and provide feedback</li>
-                <li class="main-modal__item">contact the organisers</li>
-            </ul>
+    <div class="main-modal__container">
+        <div class="main-modal__img">
+            <img class="main-modal__photo" src="<?=SITE_TEMPLATE_PATH?>/images/phone.png" alt="">
+        </div>
+        <div class="main-modal__textblock">
+            <h2 class="main-modal__title">Please, don’t forget to update your LTM Guest System app.</h2>
             <div class="appstores">
                 <a href="https://itunes.apple.com/us/app/ltm-guest-system/id972720598" target="_blank"><img src="<?=SITE_TEMPLATE_PATH?>/images/app-store.svg" alt="" class="storelogo"></a>
-                <a href="https://play.google.com/store/apps/details?id=volpis.com.ltm" target="_blank"><img src="<?=SITE_TEMPLATE_PATH?>/images/google-play.svg" alt="" class="storelogo"></a>
+                <a href="https://play.google.com/store/apps/details?id=com.ltm.meetup" target="_blank"><img src="<?=SITE_TEMPLATE_PATH?>/images/google-play.svg" alt="" class="storelogo"></a>
             </div>
         </div>
-        <p class="main-modal__footer">The App works on iOS or Android, for iPhone & iPad or smartphones & tablets. The App has been developed specifically for this and other Luxury Travel Mart events.</p>
     </div>
 </div>
+
 <div class="mdl-overlay"></div>
